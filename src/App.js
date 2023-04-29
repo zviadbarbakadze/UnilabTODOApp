abbd9882f8a8ab0074cd0fc6d272253206e25659
zviadbarbakadze <zviadbarbakadze5@gmail.com>
2023-04-29 14:57:36 +0400

    created getstarted component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import styled from "styled-components";
+import { Routes, Route } from "react-router-dom";
+import GetStarted from "./GetStarted";
+import SignIn from "./SignIn";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Container>
+      <Routes>
+        <Route index element={<GetStarted />} />
+        <Route path="/" element={<GetStarted />} />
+        <Route path="signin" element={<SignIn />} />
+      </Routes>
+    </Container>
+  );
 }
 
 export default App;
+
+const Container = styled.div``;
---
+++ src/GetStarted.js
@@ -0,0 +1,54 @@
+import React from "react";
+import styled from "styled-components";
+import element from "./assets/Group 3.svg";
+import { useNavigate } from "react-router-dom";
+
+export default function GetStarted() {
+  const navigate = useNavigate();
+  return (
+    <Container>
+      <Image src={element} alt="element" />
+      <Text>Keep Track Of Daily Tasks In Life</Text>
+      <Button onClick={() => navigate("signin")}>Get Started</Button>
+    </Container>
+  );
+}
+const Container = styled.div`
+  background-color: #000000;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  height: 100vh;
+`;
+const Image = styled.img``;
+const Text = styled.h1`
+  font-size: 54px;
+  color: #ffffff;
+  text-align: center;
+  @media (max-width: 768px) {
+    font-size: 40px;
+  }
+`;
+const Button = styled.button`
+  width: 388px;
+  height: 98px;
+  background-color: #5efc8d;
+  border-radius: 4px;
+  font-size: 48px;
+  padding: 16px 48px;
+  cursor: pointer;
+  :hover {
+    background-color: #329632;
+  }
+  @media (max-width: 768px) {
+    width: 60%;
+    font-size: 40px;
+    padding: 6px 20px;
+  }
+  @media (max-width: 500px) {
+    width: 60%;
+    font-size: 20px;
+    padding: 6px 20px;
+  }
+`;
